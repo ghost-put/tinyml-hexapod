ffc15e61ad8254744bc98b1b9bfcb62c0728fd90
v_voytus <wojciech.channel@gmail.com>
2022-05-12 04:19:33 +0200

    Further robot movement updates: 	1. Fixed bug with swapped knee and tigh pins 	2. Added Triangle class which contains data about each leg and their relative positions to one another 	3. Implemented new walking features: rotating left and right, moving backwards

--- movement/robot.py
+++ movement/robot.py
@@ -1,14 +1,12 @@
 from .leg import Leg
+from .triangle import Triangle
 from machine import Pin
 from time import sleep
 
 
 class Robot:
-    def __init__(self, leg_ids: dict, triangles: tuple, freq: int = 50, led_pin: int = 25) -> None:
-        self.freq = freq
-        self.legs = self.initialize_legs(leg_ids)
-        self.triangles = {}
-        self.initialize_triangles(triangles)
+    def __init__(self, left_triangle: Triangle, right_triangle: Triangle, led_pin: int = 25) -> None:
+        self.triangles = self.init_triangles(left_triangle, right_triangle)
         self.led_on(led_pin)
 
     @staticmethod
@@ -16,51 +14,69 @@ class Robot:
         led = Pin(led_pin, Pin.OUT)
         led(1)
 
-    def initialize_legs(self, leg_ids: dict) -> dict:
-        legs = {}
-        for leg in leg_ids:
-            knee, thigh = leg_ids[leg]
-            legs[leg] = Leg(knee, thigh, self.freq)
-        return legs
+    @staticmethod
+    def init_triangles(left_triangle: Triangle, right_triangle) -> dict:
+        return {"left": left_triangle, "right": right_triangle}
 
-    def initialize_triangles(self, triangles: tuple) -> None:
-        self.triangles["left"] = triangles[0]
-        self.triangles["right"] = triangles[1]
-
-    def set_leg_position(self, leg_id: str, knee_angle: int, thigh_angle: int) -> None:
-        self.legs[leg_id].set_position(knee_angle, thigh_angle)
+    @staticmethod
+    def set_leg_position(leg: Leg, knee_angle: int, thigh_angle: int) -> None:
+        leg.set_position(knee_angle, thigh_angle)
 
     def neutral_position(self) -> None:
-        for leg_id in self.legs:
-            self.set_leg_position(leg_id, 90, 90)
+        for leg in self.triangles["left"].legs.values():
+            leg.set_position(90, 90)
+        for leg in self.triangles["right"].legs.values():
+            leg.set_position(90, 90)
 
     def move_forward(self) -> None:
         self.move_triangle("left")
         self.move_triangle("right")
 
     def move_left(self) -> None:
-        pass
+        self.rotate_triangle("left", 150, 30)
+        self.rotate_triangle("right", 30, 150)
 
     def move_right(self) -> None:
-        pass
+        self.rotate_triangle("left", 30, 150)
+        self.rotate_triangle("right", 150, 30)
+
+    def move_backward(self) -> None:
+        self.move_triangle("left", thigh_angle = 30)
+        self.move_triangle("right", thigh_angle = 30)
 
     def move_triangle(self, triangle: str, knee_angle: int = 125, thigh_angle: int = 150) -> None:
         self.neutral_position()
         sleep(1)
         self.triangle_up(self.triangles[triangle], knee_angle)
-        # self.triangle_move(self.triangles[triangle], knee_angle, thigh_angle)
-        # self.triangle_down(self.triangles[triangle], thigh_angle)
+        self.triangle_move(self.triangles[triangle], knee_angle, thigh_angle)
+        self.triangle_down(self.triangles[triangle], thigh_angle, thigh_angle)
 
-    def triangle_up(self, triangle, knee_angle):
-        for leg_id in triangle:
-            self.set_leg_position(leg_id, knee_angle, 90)
+    def rotate_triangle(self, triangle: str, thigh_angle_middle: int, thigh_angle_rest: int, knee_angle: int = 125) -> None:
+        self.neutral_position()
+        sleep(1)
+        self.triangle_up(self.triangles[triangle], knee_angle)
+        self.triangle_rotate(self.triangles[triangle], knee_angle, thigh_angle_middle, thigh_angle_rest)
+        self.triangle_down(self.triangles[triangle], thigh_angle_middle, thigh_angle_rest)
 
-    def triangle_move(self, triangle, knee_angle, thigh_angle):
-        for leg_id in triangle:
-            self.set_leg_position(leg_id, knee_angle, thigh_angle)
+    def triangle_up(self, triangle: Triangle, knee_angle: int) -> None:
+        for leg in triangle.legs.values():
+            self.set_leg_position(leg, knee_angle, 90)
         sleep(1)
 
-    def triangle_down(self, triangle, thigh_angle):
-        for leg_id in triangle:
-            self.set_leg_position(leg_id, 90, thigh_angle)
+    def triangle_down(self, triangle: Triangle, thigh_angle_middle: int, thigh_angle_rest: int) -> None:
+        self.set_leg_position(triangle.legs["front"], 90, thigh_angle_rest)
+        self.set_leg_position(triangle.legs["middle"], 90, thigh_angle_middle)
+        self.set_leg_position(triangle.legs["back"], 90, thigh_angle_rest)
         sleep(1)
+
+    def triangle_move(self, triangle: Triangle, knee_angle: int, thigh_angle: int) -> None:
+        for leg in triangle.legs.values():
+            self.set_leg_position(leg, knee_angle, thigh_angle)
+        sleep(1)
+
+    def triangle_rotate(self, triangle: Triangle, knee_angle: int, thigh_angle_middle: int, thigh_angle_rest: int) -> None:
+        self.set_leg_position(triangle.legs["front"], knee_angle, thigh_angle_rest)
+        self.set_leg_position(triangle.legs["middle"], knee_angle, thigh_angle_middle)
+        self.set_leg_position(triangle.legs["back"], knee_angle, thigh_angle_rest)
+        sleep(1)
+
--- movement/triangle.py
+++ movement/triangle.py
@@ -1,16 +1,13 @@
 from .leg import Leg
 
-FREQ = 50
-
 
 class Triangle:
-    def __init__(self, front: tuple, middle: tuple, back: tuple):
-        self.legs = self.initialize_legs(front, middle, back)
+    def __init__(self, front: tuple, middle: tuple, back: tuple, freq: int = 50) -> None:
+        self.freq = freq
+        self.legs = self.set_triangle(front, middle, back)
 
-    @staticmethod
-    def initialize_legs(front: tuple, middle: tuple, back: tuple) -> dict:
-        legs = {"front": Leg(front[0], front[1], front[2])}
-        for leg in leg_ids:
-            knee, thigh = leg
-            legs[leg] = Leg(knee, thigh, FREQ)
-        return legs
+    def set_triangle(self, front: tuple, middle: tuple, back: tuple) -> dict:
+        return {"front": self.init_leg(front), 'middle': self.init_leg(middle), 'back': self.init_leg(back)}
+
+    def init_leg(self, leg: tuple) -> Leg:
+        return Leg(leg[0], leg[1], self.freq)
